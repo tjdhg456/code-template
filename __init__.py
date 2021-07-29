6c77bf5185119902a47cef21bc68db4b1bc5516a
seungback <shback@gist.ac.kr>
2021-07-29 08:25:09 +0000

    --

--- __init__.py
+++ __init__.py
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 "This is raeyo bug"
 
 
 sadlkfmnsdlkfmdslkfmlkds mslkdmflksmlkmlsd
-=======
-"This is raeyo bugs"
-
-
-sfkjaslfoasdjolll
->>>>>>> 577ba2b7f509f0bfe4ec7b5eafcbc84dcacb9d58
